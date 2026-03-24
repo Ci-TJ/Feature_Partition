d47632d0e8017d13da0cf57f0380d78b2dcc96e3
Qin Lin <47169253+Ci-TJ@users.noreply.github.com>
2026-03-24 22:39:00 +0800

    Add Cython extension for new binary search module

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
+import numpy as np
+
+ext = Extension(
+    name="new_binary_search_perplexity",
+    sources=["new_binary_search_perplexity.pyx"],
+    include_dirs=[np.get_include()],
+)
 
 setup(
-    ext_modules=cythonize("new_binary_search_perplexity.pyx",force=True), 
+    ext_modules=cythonize(
+        [ext],
+        language_level="3",
+        compiler_directives={"boundscheck": False, "wraparound": False, "cdivision": True},
+    )
 )
