a70f272949fd98f6ae8e3c26052171227e9f6b79
Qin Lin <47169253+Ci-TJ@users.noreply.github.com>
2026-03-24 22:38:03 +0800

    Refactor type definitions in new_binary_search_perplexity.pyx
    Removed redundant type definitions and added Cython type definitions for float32_t and float64_t.

--- new_binary_search_perplexity.pyx
+++ new_binary_search_perplexity.pyx
@@ -1,3 +1,8 @@
+# cython: boundscheck=False
+# cython: wraparound=False
+# cython: cdivision=True
+# cython: language_level=3
+
 # BSD 3-Clause License
 # 
 # Copyright (c) 2007-2024 The scikit-learn developers.
@@ -38,43 +43,15 @@
 
 import numpy as np
 
+cimport numpy as cnp 
+
 from libc cimport math
 from libc.math cimport INFINITY
 
-####################################### A copy of sklearn.utils._typedefs.pxd ################
-# Commonly used types
-# These are redefinitions of the ones defined by numpy in
-# https://github.com/numpy/numpy/blob/main/numpy/__init__.pxd.
-# It will eventually avoid having to always include the numpy headers even when we
-# would only use it for the types.
-#
-# When used to declare variables that will receive values from numpy arrays, it
-# should match the dtype of the array. For example, to declare a variable that will
-# receive values from a numpy array of dtype np.float64, the type float64_t must be
-# used.
-#
 
-ctypedef unsigned char uint8_t
-ctypedef unsigned int uint32_t
-ctypedef unsigned long long uint64_t
-#
-# intp_t/np.intp should be used to index arrays in a platform dependent way.
-# Storing arrays with platform dependent dtypes as attribute on picklable
-# objects is not recommended as it requires special care when loading and
-# using such datastructures on a host with different bitness. Instead one
-# should rather use fixed width integer types such as int32 or uint32 when we know
-# that the number of elements to index is not larger to 2 or 4 billions.
-ctypedef Py_ssize_t intp_t
-ctypedef float float32_t
-ctypedef double float64_t
-# Sparse matrices indices and indices' pointers arrays must use int32_t over
-# intp_t because intp_t is platform dependent.
-# When large sparse matrices are supported, indexing must use int64_t.
-# See https://github.com/scikit-learn/scikit-learn/issues/23653 which tracks the
-# ongoing work to support large sparse matrices.
-ctypedef signed char int8_t
-ctypedef signed int int32_t
-ctypedef signed long long int64_t
+
+ctypedef cnp.float32_t float32_t
+ctypedef cnp.float64_t float64_t
 
 #############################################################################################################
 
